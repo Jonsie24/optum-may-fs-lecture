cc006ee48103046f22dabf9b7ae162533a669c5e
mfortenberry <mfortenberry@yorksolutions.net>
2022-06-23 10:13:42 -0500

    fixed controller test running

--- src/test/java/net/yorksolutions/myfirstjavaproject/FizzBuzzControllerTest.java
+++ src/test/java/net/yorksolutions/myfirstjavaproject/FizzBuzzControllerTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -11,15 +12,15 @@ import org.springframework.web.client.RestTemplate;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class FizzBuzzControllerTest {
-    @Autowired
-    FizzBuzzController controller;
+    @LocalServerPort
+    int port;
 
     @Test
     void fizzbuzz() {
         final RestTemplate rest = new RestTemplate();
-        final ResponseEntity<String> actual = rest.getForEntity("http://localhost:8080/fizzbuzz?input=4", String.class);
+        final ResponseEntity<String> actual = rest.getForEntity("http://localhost:" + port + "/fizzbuzz?input=4", String.class);
         assertEquals(HttpStatus.OK, actual.getStatusCode());
         assertEquals("4", actual.getBody());
     }
